b08934679ba44fd235ede3238c738c35bf69f3ac
Sergey UP <sergey10goals@gmail.com>
2023-12-08 20:17:15 +0200

    add Header component

---
+++ components/Header/Header.js
@@ -0,0 +1,31 @@
+import { DivComponent } from "../../common/divComponent";
+import './header.css'
+
+export class Header extends DivComponent {
+  constructor(appState) {
+    super();
+    this.appState = appState;
+  }
+
+  render() {
+    this.element.innerHTML = '';
+    this.element.classList.add('header');
+    this.element.innerHTML = `
+      <div class="logo">
+        <img class="logo__img" src="../../public/img/logo.svg" alt="Логотип сайту">
+      </div>
+      <nav class="nav">
+        <a class="nav__item" href="">
+          <img src="../../public/img/search.svg" alt="Іконка пошуку">
+          <span>Пошук книги</span>
+        </a>
+        <a class="nav__item" href="">
+          <img src="../../public/img/favourites.svg" alt="Іконка додавання до обранного">
+          <span>Обране: ${this.appState.favourites.length}</span>
+        </a>
+      </nav>
+    `;
+
+    return this.element;
+  }
+}

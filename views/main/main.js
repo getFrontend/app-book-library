614aa40a24c4dc5c5509cf29117a3656cf32daa4
Sergey UP <sergey10goals@gmail.com>
2023-12-08 18:55:54 +0200

    add on-change library

--- views/main/main.js
+++ views/main/main.js
@@ -1,3 +1,4 @@
+import onChange from "on-change";
 import { AbstractView } from "../../common/view";
 
 export class MainView extends AbstractView {
@@ -11,7 +12,14 @@ export class MainView extends AbstractView {
   constructor(appState) {
     super();
     this.appState = appState;
+    this.appState = onChange(this.appState, this.useAppState.bind(this));
     this.setTitle('Пошук книг');
+  }
+
+  useAppState(path) {
+    if (path === 'favourites') {
+      console.log(path);
+    }
   }
 
   render() {
@@ -19,5 +27,6 @@ export class MainView extends AbstractView {
     main.innerHTML = `Кількість знайденних книг: ${this.appState.favourites.length}`;
     this.app.innerHTML = '';
     this.app.append(main);
+    this.appState.favourites.push('a');
   }
 }

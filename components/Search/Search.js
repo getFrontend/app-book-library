0c2808f9c41573e09dcab054c721dcff7ec6500d
Sergey UP <sergey10goals@gmail.com>
2023-12-09 23:49:58 +0200

    update Search component

--- components/Search/Search.js
+++ components/Search/Search.js
@@ -7,6 +7,11 @@ export class Search extends DivComponent {
     this.state = state;
   }
 
+  search() {
+    const value = this.element.querySelector('.search__input').value;
+    this.state.searchQuery = value;
+  }
+
   render() {
     this.element.classList.add('search');
     this.element.innerHTML = `
@@ -19,6 +24,12 @@ export class Search extends DivComponent {
         <img class="search__btn-img" src="../../public/img/search-white.svg" alt="Іконка пошуку">
       </button>
     `;
+    this.element.querySelector('.search__btn').addEventListener('click', this.search.bind(this));
+    this.element.querySelector('.search__input').addEventListener('keydown', (event) => {
+      if (event.code === 'Enter') {
+        this.search();
+      }
+    })
 
     return this.element;
   }

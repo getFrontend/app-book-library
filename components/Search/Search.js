69ddc961f3507ab035edb8f666f0fc9e73dbeefc
Sergey UP <sergey10goals@gmail.com>
2023-12-09 23:14:43 +0200

    update search component

--- components/Search/Search.js
+++ components/Search/Search.js
@@ -10,7 +10,14 @@ export class Search extends DivComponent {
   render() {
     this.element.classList.add('search');
     this.element.innerHTML = `
-      
+      <div class="search__wrapper">
+        <input class="search__input" type="text" placeholder="Знайти книгу або автора" value=${this.state.searchQuery ?
+        this.state.searchQuery : ""}>
+        <img class="search__icon" src="../../public/img/search.svg" alt="Іконка пошуку">
+      </div>
+      <button class="search__btn" aria-label="Пошук">
+        <img class="search__btn-img" src="../../public/img/search-white.svg" alt="Іконка пошуку">
+      </button>
     `;
 
     return this.element;

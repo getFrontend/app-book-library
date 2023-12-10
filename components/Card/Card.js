9aefffc74b8982c61a92c667db12a93ba8936a83
Sergey UP <sergey10goals@gmail.com>
2023-12-10 18:02:38 +0200

    update Card component

--- components/Card/Card.js
+++ components/Card/Card.js
@@ -1,6 +1,8 @@
 import { DivComponent } from "../../common/divComponent";
 import './card.css'
 
+const IMG_URL = 'https://covers.openlibrary.org/b/olid';
+
 export class Card extends DivComponent {
   constructor(appState, cardState) {
     super();
@@ -10,8 +12,29 @@ export class Card extends DivComponent {
 
   render() {
     this.element.classList.add('card');
+    const existInFavourites = this.appState.favourites.find(i => i.key === this.cardState.key);
+
     this.element.innerHTML = `
-      
+      <div class="card__image">
+        <img src="${IMG_URL}/${this.cardState.cover_edition_key}-M.jpg" alt="Обкладинка книги">
+      </div>
+      <div class="card__info">
+        <div class="card__tag">
+          ${this.cardState.subject ? this.cardState.subject[0] : 'жанр не вказано'}
+        </div>
+        <div class="card__name">
+          ${this.cardState.title}
+        </div>
+        <div class="card__author">
+          ${this.cardState.author_name ? this.cardState.author_name[0] : 'автор не вказаний'}
+        </div>
+        <button class="card__btn ${existInFavourites ? 'card__btn_add' : ''}">
+          ${existInFavourites
+        ? '<img src="../../public/img/favourites-white.svg" alt="Книжку вже додано в обране">'
+        : '<img src="../../public/img/favourites.svg" alt="Додати в обране">'
+      }
+        </button>
+      </div>
     `;
 
     return this.element;

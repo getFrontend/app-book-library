9af9b477c91883ff7424d22dfafb1c342ca543c1
Sergey UP <sergey10goals@gmail.com>
2023-12-11 08:49:24 +0200

    update Header component

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -11,14 +11,16 @@ export class Header extends DivComponent {
     this.element.classList.add('header');
     this.element.innerHTML = `
       <div class="logo">
-        <img class="logo__img" src="../../public/img/logo.svg" alt="Логотип сайту">
+        <a className="logo__link" href="/">
+          <img class="logo__img" src="../../public/img/logo.svg" alt="Логотип сайту">
+        </a>
       </div>
       <nav class="nav">
-        <a class="nav__item" href="">
+        <a class="nav__item" href="#search">
           <img src="../../public/img/search.svg" alt="Іконка пошуку">
           Пошук книги
         </a>
-        <a class="nav__item" href="">
+        <a class="nav__item" href="#favourites">
           <img src="../../public/img/favourites.svg" alt="Іконка додавання до обранного">
           Обране <span class="nav__item_counter">${this.appState.favourites.length}</span>
         </a>

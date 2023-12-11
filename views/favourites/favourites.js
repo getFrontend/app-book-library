29f06f075e3e15ca7d11b880979bb55d1c64db59
Sergey UP <sergey10goals@gmail.com>
2023-12-11 08:38:08 +0200

    add FavouritesView

---
+++ views/favourites/favourites.js
@@ -0,0 +1,36 @@
+import onChange from "on-change";
+import { AbstractView } from "../../common/view";
+import { Header } from "../../components/Header/Header";
+import { CardList } from "../../components/CardList/CardList";
+
+export class FavouritesView extends AbstractView {
+  constructor(appState) {
+    super();
+    this.appState = appState;
+    this.appState = onChange(this.appState, this.useAppState.bind(this));
+    this.setTitle('Список обраних книг');
+  }
+
+  destroy() {
+    onChange.unsubscribe(this.appState);
+  }
+
+  useAppState(path) {
+    if (path === 'favourites') {
+      this.render();
+    }
+  }
+
+  render() {
+    const favourites = document.createElement('div');
+    favourites.append(new CardList(this.appState, { list: this.appState.favourites }).render());
+    this.app.innerHTML = '';
+    this.app.append(favourites);
+    this.renderHeader();
+  }
+
+  renderHeader() {
+    const header = new Header(this.appState).render();
+    this.app.prepend(header);
+  }
+}
